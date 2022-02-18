b215926933ed4dcfb12846ef94308093730b481b
andrey12v <andrey12v@gmail.com>
2022-02-17 20:26:24 -0600

    Use Prompt component to display warning if we decide to leave from QuoteForm
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25600884#overview

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -3,13 +3,11 @@ import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
-  
   const history = useHistory();
 
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
     
-    //we use useHistory hook to navigate to the page we want
     history.push('/quotes');
   };
 
