dff0071a2f2f004ff82fd47205af25dd69d6e444
andrey12v <andrey12v@gmail.com>
2022-02-17 21:14:12 -0600

    Add sort parameters to URL to sort data in QuoteList component
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25600888#overview

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,50 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+//sort helper function
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  const history = useHistory();
+  //useLocation hook allows us to get URL location object consisting of path, search and other properties
+  //in our case we use this hook to get sort parameter 
+  const location = useLocation();
+
+  //URLSearchParams - the standard javascript constructor built in the browser 
+  //we use it to get parameters from the useLocation hook 
+  //queryParams variable consists of multiple parameters 
+  const queryParams = new URLSearchParams(location.search);
+
+  //here we use get method to get 'sort' parameter and set false or true depending on the condition 
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortingHandler = () => {
+    //push method allows us to the page tat was before the current one 
+    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? 'Descending' : 'Ascending'}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
