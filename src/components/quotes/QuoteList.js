9f4922271ef4492ac76aee7d7f2851c0a5fc0e7a
andrey12v <andrey12v@gmail.com>
2022-02-17 22:11:11 -0600

    Replace hard coded URLs with match.path hook, read all comments
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25600892#overview
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25600896#overview

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -4,7 +4,6 @@ import { useHistory, useLocation } from 'react-router-dom';
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
-//sort helper function
 const sortQuotes = (quotes, ascending) => {
   return quotes.sort((quoteA, quoteB) => {
     if (ascending) {
@@ -17,23 +16,21 @@ const sortQuotes = (quotes, ascending) => {
 
 const QuoteList = (props) => {
   const history = useHistory();
-  //useLocation hook allows us to get URL location object consisting of path, search and other properties
-  //in our case we use this hook to get sort parameter 
   const location = useLocation();
 
-  //URLSearchParams - the standard javascript constructor built in the browser 
-  //we use it to get parameters from the useLocation hook 
-  //queryParams variable consists of multiple parameters 
   const queryParams = new URLSearchParams(location.search);
 
-  //here we use get method to get 'sort' parameter and set false or true depending on the condition 
   const isSortingAscending = queryParams.get('sort') === 'asc';
 
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
+  //here we also use current location.pathname instead of the hard coded '/quotes?sort=' url in history hook 
+  //in this url we also use dynnamic injection ${} so that the parameters are recognized
   const changeSortingHandler = () => {
-    //push method allows us to the page tat was before the current one 
-    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${(isSortingAscending ? 'desc' : 'asc')}`
+    });
   };
 
   return (
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { useParams, Route } from 'react-router-dom';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
@@ -10,6 +10,14 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  //here we use useRouteMatch hook to get router URL as an object consisting of multiple properties - path, urls, and params
+  //path "/quotes/:quoteId" - gives us the path that we defined but not the actual path with parameter
+  //url "/quotes/q2" - gives us the actual url with parameter 
+  //with this properties we replace the path {'/quotes/${params.quoteId}/comments'} with {`${match.path}/comments`}
+  //in order to inject parameter dynamically we use this syntax ${}
+  //we also user ${match.url} to replace it in the link - '/quotes/${params.quoteId}/'
+  //with the help of `${match.path} and ${match.url} we don't have to adjust routes manually in every component where it is reused  
+  const match = useRouteMatch();
   const params = useParams();
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
@@ -18,13 +26,27 @@ const QuoteDetail = () => {
     return <p>No quote found!</p>;
   }
 
-  //here we use nested route in to output additional component - "comments"
+  //when we use Route component it allows us to display anything inside of it if it matches its route path 
+  //in our case we display Link "Load Comments" if it matches the route `/quotes/${params.quoteId}/` 
+  //we also use the word exact to match the full path instead of just the begining of the path
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      
+      <Route path={match.path} exact>
+        <div className='centered'>
+          
+          <Link className='btn--flat' to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+
+        </div>
+      </Route>
+      
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
+
     </Fragment>
   );
 };
