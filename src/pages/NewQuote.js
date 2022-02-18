c16b573fd28819935470f244fd7671f42deffd2b
andrey12v <andrey12v@gmail.com>
2022-02-17 20:09:00 -0600

    Add useHistory hook to navigate to another page
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25600874#overview

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,16 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+  
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    
+    //we use useHistory hook to navigate to the page we want
+    history.push('/quotes');
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
